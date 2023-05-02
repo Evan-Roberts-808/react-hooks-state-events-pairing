8621745563b8cb19fd1daba1c39ef38d7faa20f6
Evan <eroberts1998@icloud.com>
2023-05-02 14:52:32 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
+import React, { useState } from 'react';
 import video from "../data/video.js";
+import VideoSection from "./VideoSection.js"
+import CommentSection from "./CommentSection.js"
 
 function App() {
-  console.log("Here's your data:", video);
+  const [showComments, setShowComments] = useState(true)
+
+  function toggleComments() {
+    setShowComments(prevShowComments => !prevShowComments)
+  }
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <VideoSection video={video} toggleComments={toggleComments} showComments={showComments}/>
+      <CommentSection video={video} showComments={showComments}/>
     </div>
   );
 }
-
 export default App;
